1db8a17dd8a28245a536e40a604c5a83c393700d
PedroLuisDionisioFraga <pedrodfraga@hotmail.com>
2025-05-31 12:45:06 -0300

    :tada: First commit

---
+++ main/main.c
@@ -0,0 +1,51 @@
+/* Blink Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <driver/gpio.h>
+#include <esp_log.h>
+
+#define LED_GPIO_PIN 2
+
+static const char *TAG = "MAIN";
+static bool s_led_state = false;
+
+void configure_led(void)
+{
+    /* Configure the GPIO pin for the LED */
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << LED_GPIO_PIN),
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
+    gpio_config(&io_conf);
+}
+
+void blink_led(void)
+{
+    gpio_set_level(LED_GPIO_PIN, s_led_state ? 1 : 0);
+}
+
+void app_main(void)
+{
+
+    /* Configure the peripheral according to the LED type */
+    configure_led();
+
+    while (1) {
+        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
+        blink_led();
+        /* Toggle the LED state */
+        s_led_state = !s_led_state;
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
